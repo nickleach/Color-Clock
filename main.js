396e6ffbbfd8b2c4d8c42e115b7f9b8ff8294134
Nick Leach <nickleach22@gmail.com>
2015-05-18 17:10:06 -0400

    Added Buttons

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var box = document.querySelector('.box');
 var clock = document.querySelector('.clock');
-
+var colorButton = document.querySelector('.color_button')
+var normal = document.querySelector('.normal')
+var buttons = document.querySelector('button')
 function zero(x){
   if (x < 10){
     x = '0' + x;
@@ -13,17 +15,43 @@ function pM(x){
   }
   return x
 }
+function currentTime (){
+  var currentDate = new Date();
+  var hours = zero(pM(currentDate.getHours()));
+  var minutes = zero(currentDate.getMinutes());
+  var seconds = zero(currentDate.getSeconds());
+  var nowTime = hours + ':' + minutes + ':'+ seconds;
+  clock.innerHTML= nowTime;
+var timer = setTimeout(function(){currentTime()}, 1000)
+box.style.backgroundColor=  nowTime;
 
-var currentTime = function(){
+}
+// currentTime();
+function colorChange (){
   var currentDate = new Date();
   var hours = zero(pM(currentDate.getHours()));
   var minutes = zero(currentDate.getMinutes());
   var seconds = zero(currentDate.getSeconds());
   var color = '#' + hours + minutes + seconds;
   clock.innerHTML= color;
-var timer = setInterval(function(){currentTime()}, 1000)
-clock.style.backgroundColor= color
+var timer = setTimeout(function(){colorChange()}, 1000)
+box.style.backgroundColor= color
 }
-currentTime()
+
+// var timeE = setTimeout(function())
+
+// switch(){
+//   case (colorButton.addEventListener("click")):
+//     colorChange();
+//   break;
+//   case (normal.addEventListener("click")):
+//     currentTime();
+//   break;
+//   default:
+//      currentTime();
+// }
+// colorChange();
+
+
 
 
