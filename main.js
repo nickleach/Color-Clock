4155e6211c90bf88e2c9583fb120ac53d0bdae9b
Nick Leach <nickleach22@gmail.com>
2015-05-18 23:54:39 -0400

    Finished

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ var box = document.querySelector('.box');
 var clock = document.querySelector('.clock');
 var colorButton = document.querySelector('.color_change')
 var normal = document.querySelector('.normal')
+var army = document.querySelector('.army')
 var buttons = document.querySelector('button')
+var greeting = document.querySelector('.greeting')
+var body = document.querySelector('body')
 function zero(x){
   if (x < 10){
     x = '0' + x;
@@ -15,49 +18,50 @@ function pM(x){
   }
   return x
 }
-var currentTime;
-var colorChange;
+
+var timer = function(x) {
+  setTimeout(x)}
+var timerOne = function(x) {
+  setTimeout(x)}
+var timerTwo= function(x){
+  setTimeout(x)}
+
 function currentTime (){
-  window.clearTimeout(colorChange)
   var currentDate = new Date();
   var hours = zero(pM(currentDate.getHours()));
   var minutes = zero(currentDate.getMinutes());
   var seconds = zero(currentDate.getSeconds());
   var nowTime = hours + ':' + minutes + ':'+ seconds;
+  greeting.innerHTML= ''
   clock.innerHTML= nowTime;
-var timer = setTimeout(function(){currentTime()}, 1000)
-box.style.backgroundColor=  nowTime;
-
+  timer(currentTime, 1000)
+}
+function armyTime (){
+  var currentDate = new Date();
+  var hours = zero(currentDate.getHours());
+  var minutes = zero(currentDate.getMinutes());
+  var seconds = zero(currentDate.getSeconds());
+  var milt = hours + ':' + minutes + ':'+ seconds;
+  greeting.innerHTML= ''
+  clock.innerHTML= milt;
+  timerTwo(armyTime, 1000)
 }
 // currentTime();
 function colorChange (){
-  window.clearTimeout(currentTime)
   var currentDate = new Date();
   var hours = zero(pM(currentDate.getHours()));
   var minutes = zero(currentDate.getMinutes());
   var seconds = zero(currentDate.getSeconds());
   var color = '#' + hours + minutes + seconds;
-  clock.innerHTML= color;
-var timer = setTimeout(function(){colorChange()}, 1000)
-box.style.backgroundColor= color
+  greeting.innerHTML= ''
+  clock.innerHTML= color
+  timerOne(colorChange, 1000)
+  body.style.backgroundColor= color
 }
-colorButton.addEventListener('click', colorChange)
-normal.addEventListener('click', currentTime)
 
-// var timeE = setTimeout(function())
-
-// switch(){
-//   case (colorButton.addEventListener("click")):
-//     colorChange();
-//   break;
-//   case (normal.addEventListener("click")):
-//     currentTime();
-//   break;
-//   default:
-//      currentTime();
-// }
-// colorChange();
-
+colorButton.addEventListener('click', colorChange, clearTimeout(timer), clearTimeout(timerTwo))
+normal.addEventListener('click', currentTime, clearTimeout(timerOne), clearTimeout(timerTwo))
+army.addEventListener('click', armyTime, clearTimeout(timer), clearTimeout(timerOne))
 
 
 
